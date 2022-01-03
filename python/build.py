e4bd9684946fc14c69bb425b755cb91e02ceabbc
Ron Gonzalez <zlgonzalez@yahoo.com>
2022-01-02 18:29:33 -0800

    Python generation from xsd

--- python/build.py
+++ python/build.py
@@ -1,5 +1,6 @@
 #   -*- coding: utf-8 -*-
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, task
+import subprocess as sp
 
 use_plugin("python.core")
 use_plugin("python.unittest")
@@ -11,7 +12,11 @@ use_plugin("python.distutils")
 name = "rtsim"
 default_task = "publish"
 
-
 @init
 def set_properties(project):
     pass
+
+@task
+def generate_py():
+    sp.call(["generateDS", "-o", "src/main/python/scxmlgen/scxml.py", "-s",
+            "src/main/python/scxmlgen/scxml_app.py", "../xsd/scxml.xsd"])
